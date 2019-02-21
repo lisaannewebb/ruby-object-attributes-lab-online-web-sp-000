5c12827f03b171bb87151e04c4746d7aeb07f372
Lisa Webb <lwebbnyc@outlook.com>
2019-02-21 23:36:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 Class Person
 
-def name=(person_name)
-  @name=person_name
-end
+  def name=(person_name)
+   @name=person_name
+  end
 
-def name
-  @name
-end
+  def name
+   @name
+  end
 
 end
