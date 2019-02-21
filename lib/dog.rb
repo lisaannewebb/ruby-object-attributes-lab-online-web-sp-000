660cb41e774301e01fe13d5652d7218286519910
Lisa Webb <lwebbnyc@outlook.com>
2019-02-21 23:32:24 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog #defines class
+  
+  def name=(doggy_name) 
+    @name=doggy_name  
+  end
+  
+  def name
+  @name
+  end
+  
+  #takes argument of dog's name and sets that argument equal to a variable, "name"
+#reads the name; exposes data from inside of the object to the outside world. This is an "instance variable". 
+#call instance variable and passes string as argument to give us dog's name
+
+  def breed=(dog_type)
+    @breed=dog_type
+  end
+
+  def breed
+   @breed
+  end
+  
+end
+
+
